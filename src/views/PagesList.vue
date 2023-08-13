672cf5360c4ac7f2ac8c33e62328617e2540f622
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2023-08-13 17:18:32 +0200

    Hook with ref

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,3 +1,25 @@
 <template>
-  <p>Pages list</p>
+  <p>{{ counter }}</p>
+  <button @click.prevent="click">add</button>
 </template>
+
+<script setup>
+import { ref, reactive } from "vue";
+
+let counter = ref(0);
+function click() {
+  counter.value++;
+}
+// export default {
+//   data() {
+//     return {
+//       counter: 0,
+//     };
+//   },
+//   methods: {
+//     click() {
+//       this.counter++;
+//     },
+//   },
+// };
+</script>
