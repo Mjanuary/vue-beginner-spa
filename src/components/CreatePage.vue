ea7f4fb5994948ba6d05a787c521dbf99bf222dc
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2023-08-13 11:55:58 +0200

    Add form

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,32 +1,74 @@
 <template>
-  <div class="container mb-3">
-    <form action="">
-      <div class="mb-3">
-        <label for="" class="form-label">Page Title </label>
-        <input type="text" class="form-control" v-model="pageTitle" />
-      </div>
+  <form action="" class="container mb-3">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="mb-3">
+          <label for="" class="form-label">Page Title </label>
+          <input type="text" class="form-control" v-model="pageTitle" />
+        </div>
 
-      <div class="mb-3">
-        <label for="" class="form-label">Content </label>
-        <textarea type="text" class="form-control" rows="5"></textarea>
-      </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Content </label>
+          <textarea
+            type="text"
+            class="form-control"
+            v-model="content"
+            rows="5"
+          ></textarea>
+        </div>
 
-      <div class="mb-3">
-        <button
-          class="btn btn-primary"
-          @click.prevent="pageCreated({ pageTitle })"
-        >
-          Create Page
-        </button>
+        <div class="mb-3">
+          <button class="btn btn-primary" @click.prevent="submitForm">
+            Create Page
+          </button>
+        </div>
       </div>
-    </form>
-  </div>
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label">Link text</label>
+          <input type="text" class="form-control" v-model="linkText" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Link URL</label>
+          <input type="text" class="form-control" v-model="linkUrl" />
+        </div>
+        <div class="mb-3">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" />
+            <label class="form-check-label" for="">Published</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 <script>
 export default {
   props: ["pageCreated"],
   data() {
-    return { pageTitle: "janvier" };
+    return {
+      pageTitle: "janvier",
+      content: "",
+      linkText: "",
+      linkUrl: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("Please fill all the data");
+        return;
+      }
+
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: {
+          text: this.linkText,
+          url: this.linkUrl,
+        },
+      });
+    },
   },
 };
 </script>
