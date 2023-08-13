240c7d98d3c1f8081e48a19585dbcf3b93d6fc96
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2023-08-13 12:20:21 +0200

    use watcher

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -93,5 +93,12 @@ export default {
       this.published = true;
     },
   },
+  watch: {
+    pageTitle(newTitle, oldTitle) {
+      if (this.linkText === oldTitle) {
+        this.linkText = newTitle;
+      }
+    },
+  },
 };
 </script>
